8409770715a56c77d3322a51fca11fe2164695b9
unbrain <marsorsun@gmail.com>
2018-06-12 21:50:43 +0800

    init vue

---
+++ src/js/av.js
@@ -0,0 +1,18 @@
+
+var APP_ID = 'AMUIl1h2iLFOVlVniKNMlk1F-gzGzoHsz';
+var APP_KEY = 'APeif28DY1R82SgNa6EyeBkq';
+
+AV.init({
+    appId: APP_ID,
+    appKey: APP_KEY
+});
+
+// var TestObject = AV.Object.extend('TestObject');
+// var testObject = new TestObject();
+// testObject.save({
+//     words: 'Hello World!'
+// }).then(function (object) {
+//     alert('LeanCloud Rocks!');
+// })
+
+console.log(Vue)
