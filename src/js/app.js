db388b7d814a56216cc3bccdc7698c5ff4f293f5
unbrain <marsorsun@gmail.com>
2018-06-20 14:44:10 +0800

    登录登出注册功能

--- src/js/app.js
+++ src/js/app.js
@@ -19,9 +19,11 @@ Vue.component('editable-span', {
     },
 })
 
-var app = new Vue({
+let app = new Vue({
     el: '#app',
     data: {
+        login: false,
+        signUp: false,
         resume: {
             name: '李安',
             age: '22',
@@ -30,13 +32,76 @@ var app = new Vue({
             school: '二本',
             email: 'marsorsun@gmil.com',
             description: '心猿码意',
+            job: '前端工程师',
             about: `Now I have come to the crossroads in my life. I always knew what the right path was. Without exception, I knew. But I never
                     took it, you know why? It was too damn hard`,
+        },
+        signUpData: {
+            email: '',
+            password: '',
+        },
+        loginData: {
+            email: '',
+            password: '',   
         },
     },
     methods: {
         onEdit(key, value) { 
             this.resume[key] = value
+        },
+        onClickSave(){
+            let currentUser = AV.User.current()
+            console.log(currentUser)
+            if(!currentUser){
+                this.signUp = true
+            }else{
+                this.saveResume()
+            }
+        },
+        saveResume(){
+            let { id } = AV.User.current()
+            var todo = AV.Object.createWithoutData('User', id)
+            // 修改属性
+            todo.set('resume', this.resume)
+            // 保存到云端
+            todo.save();
+        },
+        saveUser(){
+            console.log(this.signUpData)
+            // 新建 AVUser 对象实例
+            var user = new AV.User()
+            // 设置用户名
+            user.setUsername(this.signUpData.email)
+            // 设置密码
+            user.setPassword(this.signUpData.password)
+            // 设置邮箱
+            user.setEmail(this.signUpData.email)
+            user.signUp().then(function (loggedInUser) {
+                console.log(loggedInUser)
+            }, function (error) {
+                alert(error.message)
+                console.log(error)
+                
+            });
+        },
+        loginUser(){
+            AV.User.logIn(this.loginData.email, this.loginData.password).then(function (loggedInUser) {
+                alert('success')
+                console.log(loggedInUser);
+            }, function (error) {
+                if(error.code == 211){
+                    alert('用户不存在')
+                }else if(error.code == 210){
+                    alert('邮箱和密码不匹配')
+                }
+            });
+        },
+        logOut(){
+            AV.User.logOut()
+            // 现在的 currentUser 是 null 了
+            var currentUser = AV.User.current()
+            window.location.reload
+            console.log("用户已登出")
         },
     },
 })
