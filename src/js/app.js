6472aeaface8f469c8d84f43bbb08811c47b2d78
unbrain <marsorsun@gmail.com>
2018-06-21 19:52:32 +0800

    技能项目的增删与编辑

--- src/js/app.js
+++ src/js/app.js
@@ -36,6 +36,20 @@ let app = new Vue({
             job: '前端工程师',
             about: `Now I have come to the crossroads in my life. I always knew what the right path was. Without exception, I knew. But I never
                     took it, you know why? It was too damn hard`,
+            skills: [
+                { name: 'HTML5 & CSS3', description: '能独立制作精美网页，掌握 CSS 3 动画、过渡效果、响应式等常用技术。' },
+                { name: '移动端页面 ', description: '会使用 REM、vw/vh、FastClick 等技术制作适配手机设备的页面。' },
+                { name: 'jQuery', description: '熟悉 jQuery 的常用 API，能使用 jQuery 制作网站、轮播、Tab 组件等。' },
+                { name: 'Vue', description: '熟悉 Vue 常用功能，如组件、Vue-Router、双向绑定等。' },
+                { name: '前端框架', description: '理解 MVC、MVVM 等思想，可以熟练使用相关的库。' },
+            ],
+            works: [
+                { name: '极简画板', link: 'https://github.com/unbrain/myCanvas', codeLink: 'https://unbrain.github.io/myCanvas/', skills: '原生 JavaScript、Canvas、响应式，iconfont', description: '该项目使用原生JS实现，主要调用 Canvas API，实现了划线、调色与粗细、橡皮擦、保存和清屏功能。对移动端进行兼容。' },
+                { name: '键盘书签 ', skills: '原生 JavaScript、LocalStorage', description: '该项目使用原生 JS，动态生成键盘，使用LocalStorage 保存书签。' },
+                { name: 'F&I CSS', skills: 'CSS', description: '该项目使用 CSS 实现好看的图标及有意思的动态效果。' },
+                { name: '无脸人', skills: '原生 JavaScript、响应式，Prism.js, CSS annimation', description: '该项目使得代码展示在屏幕，并根据代码画出了一个无脸人' },
+                { name: '网易云音乐', skills: 'jQuery，leandcloud，七牛云', description: '该项目使用 jQuery 模仿网易云音乐手机端，完成手机端功能并写有 PC 端后台。' },
+            ],
         },
         signUpData: {
             email: '',
@@ -49,10 +63,22 @@ let app = new Vue({
             objectId: undefined,
             email: '',
         },
+
     },
     methods: {
         onEdit(key, value) {
-            this.resume[key] = value
+            // skill[2].name
+            let reg = /\[(\d+)\]/g
+            key = key.replace(reg, (match, number) => '.' + number)
+            let keys = key.split('.')
+            let result = this.resume
+            for (let i = 0; i < keys.length; i++) {
+                if (i === keys.length - 1) {
+                    result[keys[i]] = value
+                } else {
+                    result = result[keys[i]]
+                }
+            }
         },
         onClickSave() {
             let currentUser = AV.User.current()
@@ -121,10 +147,22 @@ let app = new Vue({
         getResume() {
             var query = new AV.Query('User')
             query.get(this.currentUser.objectId).then((user) => {
-                Object.assign(this.resume, user.toJSON().resume) 
+                Object.assign(this.resume, user.toJSON().resume)
             }, (error) => {
                 // 异常处理
             });
+        },
+        appendSkill() {
+            this.resume.skills.push({ name: '技能名', description: '技能描述' })
+        },
+        minusSkill(index) {
+            this.resume.skills.splice(index, 1)
+        },
+        appendWork() {
+            this.resume.works.push({ name: '项目名称', skills: '技术点', description: '描述' })
+        },
+        minusWork(index) {
+            this.resume.works.splice(index, 1)
         },
     },
 })
