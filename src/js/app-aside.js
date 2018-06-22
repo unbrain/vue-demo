9b5afd4d36e639022e5f9a0d5b7577a7bce5db1f
unbrain <marsorsun@gmail.com>
2018-06-22 19:38:00 +0800

    登录后才能分享

--- src/js/app-aside.js
+++ src/js/app-aside.js
@@ -1,18 +1,27 @@
 Vue.component('app-aside', {
-    props:['currentUser'],
-    template:`
+    props: ['currentUserId'],
+    methods: {
+        share() {
+            if (!currentUser) {
+                this.$emit('on-click-save')
+            } else {
+                this.$emit('share')
+            }
+        },
+    },
+    template: `
         <aside>
             <div class="up">
-                <div @click="$emit('onClickSave')">保存</div>
-                <div @click="$emit('share')">分享</div>
+                <div @click="$emit('on-click-save')">保存</div>
+                <div @click="share">分享</div>
                 <div>换肤</div>
                 <div @click="window.print()">打印</div>
             </div>
 
 
             <div class="down" v-cloak>
-                <div @click="$emit('logout')" v-show="currentUser.objectId">登出</div>
-                <div @click="$emit('signup')" v-show="!currentUser.objectId">注册</div>
+                <div @click="$emit('logout')" v-show="currentUserId">登出</div>
+                <div @click="$emit('signup')" v-show="!currentUserId">注册</div>
             </div>
         </aside>
     `
